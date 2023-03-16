75e3558f14ea4f2dc4a198f79ffc9a22d708921e
Sianernian <sianernian@qq.com>
2023-03-16 09:59:42 +0800

    生成二叉树

--- tree/TreeNode.go
+++ tree/TreeNode.go
@@ -1,7 +1,33 @@
 package main
 
+import "fmt"
+
 type TreeNode struct {
 	Val	int
 	Left *TreeNode
 	Right *TreeNode
+}
+
+
+func (node *TreeNode)print(){
+	if node ==nil{
+		fmt.Println("nil=node")
+	}else {
+		fmt.Println(node.Val)
+	}
+}
+
+
+func (node *TreeNode)setVal(val int){
+	node.Val =val
+}
+
+func (node *TreeNode)recu(){
+	if node !=nil{
+		node.print()
+		node.Left.recu()
+		node.Right.recu()
+	}else {
+		return
+	}
 }
